91f06fee61277ea553904134226e780bef81a568
ae95medina <ae95medina@gmail.com>
2022-04-18 09:21:03 -0500

    EventListener

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2");
 h2.textContent = "*JavaScript has joined the chat*";
 document.querySelector("body").appendChild(h2);
+
+const p1 = document.createElement("p1");
+p1.textContent = "Click anywhere on this webpage for a surprise!";
+document.querySelector("body").appendChild(p1)
+
+document.addEventListener("click", function(){
+    document.body.style.backgroundColor = "lightblue";
+  });
